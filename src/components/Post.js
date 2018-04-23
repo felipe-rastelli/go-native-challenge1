a8655375803161c2b946f5fb246c3ec53bae0440
Rastelli <felipe-rastelli@hotmail.com>
2018-04-23 03:09:07 -0300

    Post component added. Main view with scroll added.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import { View, TextView, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    marginVertical: 10,
+    marginHorizontal: 20
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#EEEEEE',
+    marginVertical: 10
+  },
+  title: {
+    color: '#333333',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  author: {
+    color: '#999999'
+  },
+  description: {
+    color: '#666666'
+  }
+});
+
 const Post = ({ title, author, description }) => (
-  <View>
+  <View style={styles.container}>
+    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.author}>{author}</Text>
+    <View style={styles.divider} />
+    <Text style={styles.description}>{description}</Text>
   </View>
 );
+
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Post;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
-  View,
+  ScrollView,
+  View
 } from 'react-native';
 
 import Post from 'components/Post';
@@ -13,31 +13,58 @@ import 'config/DevToolsConfig';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#EE7777',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+  scrollContainer: {
+    paddingVertical: 10
+  }
 });
 
 export default class App extends Component {
   state = {
-
+    posts: [
+      {
+        id: 1,
+        title: 'Aprendendo React Native',
+        author: 'Diego Schell Fernandes',
+        description: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica '
+          + 'e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido '
+          + 'pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+      },
+      {
+        id: 2,
+        title: 'Aprendendo React Native',
+        author: 'Diego Schell Fernandes',
+        description: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica '
+          + 'e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido '
+          + 'pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+      },
+      {
+        id: 3,
+        title: 'Aprendendo React Native',
+        author: 'Diego Schell Fernandes',
+        description: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica '
+          + 'e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido '
+          + 'pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+      }
+    ]
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          { this.state.posts.map(post => (
+            <Post
+              key={post.id}
+              title={post.title}
+              author={post.author}
+              description={post.description}
+            />
+          )) }
+        </ScrollView>
       </View>
     );
   }
